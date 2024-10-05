4a4e3135e075fb7fd11427b19fb3540a4e622fca
AdityaVerma10 <adityaverma9575@gmail.com>
2024-10-05 18:53:22 +0530

    some refactor on module component

--- src/components/home/hero-section/module-details/index.tsx
+++ src/components/home/hero-section/module-details/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeadingWithDescription = ({ heading, description }) => {
   return (
-    <div className="flex flex-col gap-6 w-[35%]">
+    <div className="flex flex-col gap-6 w-[40%]">
       <p
         className=" text-[40px] text-start font-semibold "
         style={{
@@ -20,47 +20,31 @@ const HeadingWithDescription = ({ heading, description }) => {
     </div>
   );
 };
+
+const ImageBox = ({ imgUrl }) => {
+  return (
+    <div className="w-[500px] h-[380px] rounded-[10px] shadow-[0_0_15px_1px_rgba(0,0,0,0.5)] overflow-hidden ursor-pointer">
+      <img
+        src={imgUrl}
+        alt=""
+        className=" object-cover w-[110%] h-[105%] mb-10"
+      />
+    </div>
+  );
+};
 function ModuleDetails({ imgUrl, heading, description, index }) {
   return (
-    <div className="flex gap-12 justify-center cursor-pointer">
+    <div className="flex gap-12 justify-center ">
       {index % 2 != 0 ? (
         <>
-          {/* <div className="flex flex-col gap-6 w-[50%]">
-            <p className=" text-[40px] text-start font-semibold text-module-heading-color">
-              {heading}
-            </p>
-            <p className="text-[24px] font-normal text-blue-text-secondary text-start">
-              {description}
-            </p>
-          </div> */}
           <HeadingWithDescription heading={heading} description={description} />
 
-          <div className="w-[500px] h-[380px] rounded-[10px] shadow-[0_0_15px_1px_rgba(0,0,0,0.5)] overflow-hidden">
-            <img
-              src={imgUrl}
-              alt=""
-              className=" object-cover w-[110%] h-[105%] mb-10"
-            />
-          </div>
+          <ImageBox imgUrl={imgUrl} />
         </>
       ) : (
         <>
-          <div className="w-[500px] h-[380px] rounded-[10px] shadow-[0_0_15px_1px_rgba(0,0,0,0.5)] overflow-hidden">
-            <img
-              src={imgUrl}
-              alt=""
-              className=" object-cover w-[110%] h-[105%] mb-10"
-            />
-          </div>
+          <ImageBox imgUrl={imgUrl} />
 
-          {/* <div className="flex flex-col gap-6 w-[50%]">
-            <p className=" text-[40px] text-start font-semibold text-module-heading-color">
-              {heading}
-            </p>
-            <p className="text-[24px] font-normal text-blue-text-secondary text-start">
-              {description}
-            </p>
-          </div> */}
           <HeadingWithDescription heading={heading} description={description} />
         </>
       )}
